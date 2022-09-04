fe2226c598a6aa23f17a6d325ae62d4807c04bb5
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-09-04 21:51:46 +0300

    Education updated

--- Controllers/EducationController.cs
+++ Controllers/EducationController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using Project_Personel_Demo.Models;
 
 namespace Project_Personel_Demo.Controllers
@@ -11,7 +12,7 @@ namespace Project_Personel_Demo.Controllers
     public class EducationController : Controller
     {
         // GET: Education
-         DbPersonelEntities dbPersonelEntities = new DbPersonelEntities();
+        DbPersonelEntities dbPersonelEntities = new DbPersonelEntities();
         public ActionResult Index()
         {
             var educations = dbPersonelEntities.TblEducation.ToList();
@@ -29,7 +30,7 @@ namespace Project_Personel_Demo.Controllers
             dbPersonelEntities.TblEducation.Add(education);
             dbPersonelEntities.SaveChanges();
             return RedirectToAction("Index");
-        }   
+        }
 
         public ActionResult DeleteEducation(int id)
         {
@@ -38,12 +39,68 @@ namespace Project_Personel_Demo.Controllers
             dbPersonelEntities.SaveChanges();
             return RedirectToAction("Index");
         }
+        string startDateYear, endDateYear, startDateMonth, endDateMonth;
 
         [HttpGet]
         public ActionResult EditEducation(int id)
         {
+            List<string> months = new List<string>();
+            months.Add("Ocak");
+            months.Add("Şubat");
+            months.Add("Mart");
+            months.Add("Nisan");
+            months.Add("Mayıs");
+            months.Add("Haziran");
+            months.Add("Temmuz");
+            months.Add("Ağustos");
+            months.Add("Eylül");
+            months.Add("Ekim");
+            months.Add("Kasım");
+            months.Add("Aralık");
+
             var education = dbPersonelEntities.TblEducation.Find(id);
             ViewBag.date = education.EduationDate;
+
+            //tarihleri alıp arada tire var diye bölüyorum(start,end)
+            var startDate = education.EduationDate.Split('-')[0];
+            var endDate = education.EduationDate.Split('-')[1];
+            for (int i = 0; i < months.Count; i++)
+            {
+                if (startDate.Contains(months[i]))
+                {
+                    if (i < 10)
+                        startDateMonth = "0" + (i + 1);
+                    else
+                        startDateMonth = (i + 1).ToString();
+                }
+                if (endDate.Contains(months[i]))
+                {
+                    if (i < 10)
+                        endDateMonth = "0" + (i + 1);
+                    else
+                        endDateMonth = (i + 1).ToString();
+
+                }
+            }
+
+            for (int i = 0; i < startDate.Length; i++)
+            {
+                if (Char.IsNumber(startDate[i]))
+                {
+                    startDateYear += startDate[i].ToString();
+                }
+            }
+
+            for (int i = 0; i < endDate.Length; i++)
+            {
+                if (Char.IsNumber(endDate[i]))
+                {
+                    endDateYear += endDate[i].ToString();
+                }
+            }
+
+            ViewBag.startDate = startDateYear + "-" + startDateMonth;
+            ViewBag.endDate = endDateYear + "-" + endDateMonth;
             return View(education);
         }
 
